e32e6ff5283a214e30a61504f6c9785e4794177f
pierpaolozinisal <pierpaolozini@outlook.es>
2019-04-23 20:40:37 +0100

    Utilizzo di app-user  e del comp. input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  users=['Peter', 'James','John'];
+  users=['Peter', 'James','John', 'Camerun', 'Bruce'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HolaMundoComponent
+    HolaMundoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
