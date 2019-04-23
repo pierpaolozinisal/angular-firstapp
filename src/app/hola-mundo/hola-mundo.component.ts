d628fe6e0d8ca4904cb779606886fd72c594fb77
pierpaolozinisal <pierpaolozini@outlook.es>
2019-04-23 13:21:51 +0100

    My fi rst app in angular 1 step

---
+++ src/app/hola-mundo/hola-mundo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hola-mundo',
+  templateUrl: './hola-mundo.component.html',
+  styleUrls: ['./hola-mundo.component.css']
+})
+export class HolaMundoComponent implements OnInit {
+
+  title = '¡Hola Mundo!';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
