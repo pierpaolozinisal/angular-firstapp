8bdea94ee7069753e7399733d5d23639635a3774
pierpoaolozinisal <pierpaolozini@outlook.es>
2019-04-29 15:12:44 +0100

    Evento Submit per inserire input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   users: string[] = ['Ryan','Joe', 'Cameron'];
- 
-  sayHello(){
 
+  addUser(newUser){
+    //console.log(newUser.value);
+    this.users.push(newUser.value);
+    newUser.value='';
+    newUser.focus();
+    return false;
+
+  }
+  
+  sayHello(){
     alert("Hello!");
   };
   deleteUser(user){
