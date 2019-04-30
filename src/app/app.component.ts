1b98b34383f70a6dd62280f0b9a1b3e4f388495a
pierpoaolozinisal <pierpaolozini@outlook.es>
2019-04-30 17:22:13 +0100

    Ricezione dati da servizio Json esterno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,27 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  users: string[] = ['Ryan','Joe', 'Cameron'];
 
-  addUser(newUser){
-    //console.log(newUser.value);
+  name: string = 'John Carter';
+  age: number = 28;
+
+  users: string[] = ['Ryan', 'Joe', 'Cameron'];
+  posts = [];
+
+  constructor(private  dataService: DataService) {
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+  }
+
+
+
+
+  addUser(newUser) {
+    // console.log(newUser.value);
     this.users.push(newUser.value);
-    newUser.value='';
+    newUser.value = '';
     newUser.focus();
     return false;
 
   }
-  
-  sayHello(){
-    alert("Hello!");
-  };
-  deleteUser(user){
-    for(let i=0;i < this.users.length;i++){
-      if(user=== this.users[i]){
-        this.users.splice(i,1);
+
+  sayHello() {
+    alert('Hello!');
+  }
+  deleteUser(user) {
+    for ( let i = 0; i < this.users.length; i++) {
+      if ( user === this.users[i]) {
+        this.users.splice(i, 1);
       }
     }
   }
-    
-};
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 import { UserComponent } from './user/user.component';
 
+import { DataService } from './data.service';
+// DataService è un servizio e quindi appare nelll'array providers
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,9 +18,11 @@ import { UserComponent } from './user/user.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-   
+    FormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
