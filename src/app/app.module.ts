5b08323a9cf04b782194c49bd4f8db536e7b69bb
pierpoaolozinisal <pierpaolozini@outlook.es>
2019-04-30 20:56:52 +0100

    Utilizzo route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Route } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 import { UserComponent } from './user/user.component';
 
+
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
 // DataService è un servizio e quindi appare nelll'array providers
+
+const routes: Route[] = [
+  {path: '', component: HolaMundoComponent},
+  {path: 'about', component: AboutComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HolaMundoComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
 
   ],
   providers: [DataService],
